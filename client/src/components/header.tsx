6285f0f8e043129445a8b3916ca4171904d36ba1
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-09 16:18:08 +0000

    Created header and nav components

---
+++ client/src/components/header.tsx
@@ -0,0 +1,10 @@
+import Nav from "./navigation"
+
+export const Header: React.FC = () => {
+    return (
+        <header>
+            <p>Fakelandia Justice Department</p>
+            <Nav />
+        </header>
+    )
+}
---
+++ client/src/components/navigation.tsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom"
+
+const Nav : React.FC = () => {
+    return (
+        <nav>
+            <ul>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/misdemeanour'>Misdemeanour</NavLink></li>
+                <li><NavLink to='/confession'>Confession</NavLink></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
