724b586df5f153e9acf48a50f4960bb479e30ee9
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-10 17:56:56 +0000

    feat: wip added fetch and state to misdemeanour

--- client/src/components/misdemeanour.tsx
+++ client/src/components/misdemeanour.tsx
@@ -1,1 +1,17 @@
-export const Misdemeanours : React.FC = () => <>Misdemeanours!</>;
+import { useState } from 'react';
+import { Misdemeanour } from '../types/misdemeanours.types';
+
+export const Misdemeanours : React.FC = () => {
+    const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
+
+    const getMisdemeanours = async () => {
+        const amount = 2;
+        const response = await fetch(`http://localhost:8080/api/misdemeanours/${amount}`);
+        const responseJSON = await response.json();
+        setMisdemeanours(responseJSON.misdemeanours);
+    };
+
+    return (
+        <>Misdemeanours!</>
+    )
+}
