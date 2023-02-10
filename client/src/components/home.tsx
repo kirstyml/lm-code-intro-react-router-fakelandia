8b05f61ad0e3d04e3c580d70ac7769d14b482429
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-10 15:42:57 +0000

    Updated home component for required text

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,1 +1,6 @@
-export const Home : React.FC = () => <>Home!</>;
+export const Home : React.FC = () => (
+    <>
+        <p>Welcome to the home of the Justice Department of Fakelandia</p>
+        <p>Here you can browse a list of recent misdemeanours committed by our citizens or you can confess your own misdemeanour</p>
+    </>
+);
