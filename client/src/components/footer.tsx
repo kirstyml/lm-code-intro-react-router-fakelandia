a8836961ca95c575780d703156aa258c6bda481d
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-10 15:42:14 +0000

    Created footer

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer : React.FC = () => {
+    return (
+        <footer className="footer">
+            <p className="footer__copyright">© 2023 TechReturners</p>
+        </footer>
+    )
+}
