2d7dd1422837171a379cc1218380d540c6c97cce
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-13 09:51:25 +0000

    feat: useEffect calling misdemeanour endpoint and page displaying info received

--- client/src/components/misdemeanour.tsx
+++ client/src/components/misdemeanour.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Misdemeanour } from '../types/misdemeanours.types';
 
 export const Misdemeanours : React.FC = () => {
     const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
 
+    useEffect(() => {
+        getMisdemeanours();
+    }, []);
+
     const getMisdemeanours = async () => {
+        // TODO: decide how amount is specified
         const amount = 2;
         const response = await fetch(`http://localhost:8080/api/misdemeanours/${amount}`);
         const responseJSON = await response.json();
@@ -12,6 +17,19 @@ export const Misdemeanours : React.FC = () => {
     };
 
     return (
-        <>Misdemeanours!</>
+        <>
+        {
+            misdemeanours.map((misdemeanour) => {
+                return(
+                    // Refactor misdemeanour into component
+                    <div>
+                        <p>{misdemeanour.citizenId}</p>
+                        <p>{misdemeanour.misdemeanour}</p>
+                        <p>{misdemeanour.date}</p>
+                    </div>
+                )
+            })
+        }
+        </>
     )
 }
