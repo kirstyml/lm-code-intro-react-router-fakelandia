1f763460b6a1d8b91187ab1456974db448d66d87
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-16 10:01:13 +0000

    feat: added basic html for confessions page

--- client/src/components/confession_page.tsx
+++ client/src/components/confession_page.tsx
@@ -1,1 +1,24 @@
-export const Confession : React.FC = () => <>Confession!</>;
+import { MISDEMEANOURS } from "../types/misdemeanours.types"
+
+export const Confession : React.FC = () => {
+    return (
+        <div>
+            <p>It's very difficult to catch people committing misdemeanours 
+                so we appreciate it when citizens confess to us directly.</p>
+            <p>However, if you're just having a hard day and need to vent then 
+                you're welcome to contact us here too. Up to ypu!</p>
+            <form action="">
+                <label htmlFor="subject">Subject</label>
+                <input name="subject" type="text" />
+                <label htmlFor="reason">Reason for contact</label>
+                <select name="reason" id="reason">
+                <option value="">--Please select an option--</option>
+                    {MISDEMEANOURS.map(m => <option value={m}>{m}</option>)}
+                    <option value="I just want to talk">I just want to talk</option>
+                </select>
+                <textarea name="" id="" cols={30} rows={10}></textarea>
+                <input type="submit" value="Confess" />
+            </form>
+        </div>
+    )
+}
