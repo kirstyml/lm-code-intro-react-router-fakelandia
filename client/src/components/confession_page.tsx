0613256ee25678418c587b06f3676b93269e82c6
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-16 17:03:32 +0000

    feat: added handle submit and fetch call wip

--- client/src/components/confession_page.tsx
+++ client/src/components/confession_page.tsx
@@ -8,6 +8,7 @@ export const Confession : React.FC = () => {
     const [reason, setReason] = useState<reasonOptions>("");
     const [details, setDetails] = useState("");
     const [errors, setErrors] = useState({subject: false, reason: false, details: false});
+    const [submitError, setSubmitError] = useState<string | undefined>(undefined);
 
 
     const subjectValid = subject !== "";
@@ -34,13 +35,43 @@ export const Confession : React.FC = () => {
         event.target.value.length < 20 ? setErrors({...errors, details: true }) : setErrors({...errors, details: false });
     }
 
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        //validate entries ? or no need as submit button disabled until then?
+        // fetch call to send the data
+        try {
+            const response = await fetch(`http://localhost:8080/api/confess`, {
+                method: "POST",
+                body: JSON.stringify({
+                  subject: subject,
+                  reason: reason,
+                  details: details,
+                }),
+              }); 
+            const responseJSON = await response.json();
+            // should response.JSON have a type?
+            console.log(responseJSON);
+            // failure - show error message
+            if (!responseJSON.success) {
+                setSubmitError(responseJSON.message);
+            } else {
+                if (responseJSON.justTalked) {
+                    //add the misdemeanour to the state
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
         <div>
             <p>It's very difficult to catch people committing misdemeanours 
                 so we appreciate it when citizens confess to us directly.</p>
             <p>However, if you're just having a hard day and need to vent then 
                 you're welcome to contact us here too. Up to ypu!</p>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <SubjectInput handleChange={handleSubjectChange} />
                 {errors.subject && <p>Error: You must enter a subject</p>}
                 <ReasonsSelect handleChange={handleReasonChange} />
@@ -48,6 +79,7 @@ export const Confession : React.FC = () => {
                 <textarea name="details" id="" cols={30} rows={10} onChange={(event) => handleDetailsChange(event)}></textarea>
                 {errors.details && <p>Error: You must enter at least 20 characters in the details section</p>}
                 <input type="submit" value="Confess" disabled={!allValid} />
+                {submitError && <p>Error: {submitError} </p>}
             </form>
         </div>
     )
