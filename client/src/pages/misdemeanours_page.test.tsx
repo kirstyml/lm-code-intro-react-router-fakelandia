a48a0891f8680b4da9f4e4e55e0b1ca1dc9e024d
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-18 16:23:57 +0000

    Added misdemeanour page tests to test context provides correct data

---
+++ client/src/pages/misdemeanours_page.test.tsx
@@ -0,0 +1,71 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { Misdemeanours } from './misdemeanours_page';
+import { MisdemeanoursProvider } from '../context/misdemeanour_provider';
+
+const handlers = [
+    rest.get(`http://localhost:8080/api/misdemeanours/10`, (req,res,ctx) => {
+        return res(ctx.json({
+            misdemeanours: [
+                {
+                    "citizenId": 7449,
+                    "misdemeanour": "vegetables",
+                    "date": "2/18/2023"
+                    },
+                    {
+                    "citizenId": 14281,
+                    "misdemeanour": "rudeness",
+                    "date": "2/18/2023"
+                    }
+            ]
+        }))
+    }),
+    rest.get("https://picsum.photos/v2/list", (req,res,ctx) => {
+        return res(ctx.json([
+            {
+                "id": "0",
+                "author": "Alejandro Escamilla",
+                "width": 5000,
+                "height": 3333,
+                "url": "https://unsplash.com/photos/yC-Yzbqy7PY",
+                "download_url": "https://picsum.photos/id/0/5000/3333"
+                },
+                {
+                "id": "1",
+                "author": "Alejandro Escamilla",
+                "width": 5000,
+                "height": 3333,
+                "url": "https://unsplash.com/photos/LNRyGwIJr5c",
+                "download_url": "https://picsum.photos/id/1/5000/3333"
+                },
+        ]))
+    })
+];
+const server = setupServer(...handlers);
+
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('renders the table headings', () => {
+    render(<Misdemeanours />);
+    const tableHeader = screen.getByText(/Citizen ID/i);
+    expect(tableHeader).toBeInTheDocument();
+});
+
+test('loads and displays the misdemeanours', async () => {
+    render(<MisdemeanoursProvider><Misdemeanours /></MisdemeanoursProvider>);
+    await waitFor(() => screen.findByText("vegetables"));
+    expect(screen.getByText("rudeness")).toBeInTheDocument();
+});
+
+test('loads and display the punishment ideas', async () => {
+    render(<MisdemeanoursProvider><Misdemeanours /></MisdemeanoursProvider>);
+    await waitFor(() => screen.findByText("vegetables"));
+    expect(screen.getAllByAltText('punishment idea')[0]).toHaveAttribute('src', 'https://picsum.photos/id/0/100');
+})
