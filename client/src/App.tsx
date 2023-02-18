23d848d0fa91270d7ab4da98735adf00edd67ff9
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-18 16:20:57 +0000

    created folder for contexts and moved misdemeanour context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import SiteRouter from "./router";
-import { MisdemeanoursProvider } from "./components/misdemeanour_provider";
+import { MisdemeanoursProvider } from "./context/misdemeanour_provider";
 
 import './App.css'
 
