1cf2f32aa2edd0708c016aff099b286eb46b011e
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-18 12:13:00 +0000

    moved home page to new pages folder

--- client/src/components/home_page.tsx
+++
@@ -1,6 +0,0 @@
-export const Home : React.FC = () => (
-    <>
-        <p>Welcome to the home of the Justice Department of Fakelandia</p>
-        <p>Here you can browse a list of recent misdemeanours committed by our citizens or you can confess your own misdemeanour</p>
-    </>
-);
