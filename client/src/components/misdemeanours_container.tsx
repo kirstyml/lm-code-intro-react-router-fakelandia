f85bd300da9344fdb3dbef8d127a9e1a571f3615
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-21 08:54:56 +0000

    fix: removed redundant div

--- client/src/components/misdemeanours_container.tsx
+++ client/src/components/misdemeanours_container.tsx
@@ -2,21 +2,18 @@ import { Misdemeanour } from '../types/misdemeanours.types';
 import { Punishment } from '../types/punishment.types';
 import { MisdemeanourItem } from './misdemeanour';
 
-export const MisdemeanoursContainer : React.FC<{misdemeanours: Misdemeanour[], punishments: Punishment[]}> = ({ misdemeanours, punishments }) => {
+export const MisdemeanoursContainer: React.FC<{ misdemeanours: Misdemeanour[], punishments: Punishment[] }> = ({ misdemeanours, punishments }) => {
     return (
-        <>
-            <div className="misdemeanour_container">
-                <div className="misdemeanour_container--headings misdemeanour">
-                    <p className="misdemeanour__id">Citizen ID</p>
-                    <p className="misdemeanour__date">Date</p>
-                    <p className="misdemeanour__misdemeanour">Misdemeanour</p>
-                    <p className="misdemeanour__punishment">Punishment Idea</p>
-                </div>
-                {
-                    misdemeanours.map((item, index) => <MisdemeanourItem key={item.citizenId} misdemeanour={item} punishment={punishments.length - index > 0 ? punishments[index] : punishments[0]} />)
-                }
+        <div className="misdemeanour_container">
+            <div className="misdemeanour_container--headings misdemeanour">
+                <p className="misdemeanour__id">Citizen ID</p>
+                <p className="misdemeanour__date">Date</p>
+                <p className="misdemeanour__misdemeanour">Misdemeanour</p>
+                <p className="misdemeanour__punishment">Punishment Idea</p>
             </div>
-        </>
-
+            {
+                misdemeanours.map((item, index) => <MisdemeanourItem key={item.citizenId} misdemeanour={item} punishment={punishments.length - index > 0 ? punishments[index] : punishments[0]} />)
+            }
+        </div>
     )
 }
