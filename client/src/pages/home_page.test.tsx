8461453d67e09adcaa1b759ede4bcf338fb5f61a
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-21 13:09:47 +0000

    fix: removed unecessary code from Home tests

--- client/src/pages/home_page.test.tsx
+++ client/src/pages/home_page.test.tsx
@@ -1,13 +1,9 @@
-/**
- * @jest-environment jsdom
- */
- import { render, screen } from '@testing-library/react';
- import { BrowserRouter } from 'react-router-dom';
- import { Home } from './home_page';
- 
- 
- test('renders the welcome text', () => {
-   render(<BrowserRouter><Home /></BrowserRouter>);
-   const introElement = screen.getByText(/Welcome to the home/i);
-   expect(introElement).toBeInTheDocument();
- });
+import { render, screen } from '@testing-library/react';
+import { Home } from './home_page';
+
+
+test('renders the welcome text', () => {
+    render(<Home />);
+    const introElement = screen.getByText(/Welcome to the home/i);
+    expect(introElement).toBeInTheDocument();
+});
