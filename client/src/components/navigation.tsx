69569f1dd9684f365fee7b16c4155a09e6265356
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-23 15:23:28 +0000

    responsive navigation

--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -1,9 +1,14 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { ReactComponent as Menu } from "../assets/menu.svg";
 
 const Nav: React.FC = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
     return (
         <nav className="nav-bar">
-            <ul className="nav-bar__links">
+            <button className="nav-menu" onClick={() => setIsExpanded(!isExpanded)}><Menu height={40} width={40} /></button>
+            <ul className={isExpanded? "nav-bar__links--expanded" : "nav-bar__links"}>
                 <li className="nav-bar__link">
                     <NavLink to='/' className={({ isActive }) =>
                         isActive ? "nav-bar__link-text--active" : "nav-bar__link-text"
