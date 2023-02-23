d38697c3bca512496fcdc916f88914d26da1ee37
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-23 16:57:01 +0000

    refactored validation and error messages in confess page

--- client/src/components/reasonsSelect.tsx
+++ client/src/components/reasonsSelect.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { ConfessionChangeHandler } from "../types/confession.types";
 import { reasons, reasonOptions } from "../types/reasons.types";
 
 interface IReasonsSelect {
     selectedReason: reasonOptions,
-    handleChange : ConfessionChangeHandler
+    handleChange : ConfessionChangeHandler,
+    error: undefined | string
 }
 
-export const ReasonsSelect : React.FC<IReasonsSelect> = ({ selectedReason, handleChange }) => {
+export const ReasonsSelect : React.FC<IReasonsSelect> = ({ selectedReason, handleChange, error }) => {
+    const [touched, setTouched] = useState<boolean>(false);
+
     return (
         <>
             <label htmlFor="reason">Reason for contact</label>
-            <select name="reason" id="reason" value={selectedReason} onChange={(e) => handleChange(e.target.value as reasonOptions, "reason")}>
+            <select name="reason" id="reason" value={selectedReason} onChange={(e) => {setTouched(true); handleChange(e.target.value as reasonOptions, "reason")}}>
                 {reasons.map(reason => <option key={reason} value={reason}>{
                     reason === "" ? "--Please select an option--" : reason === "just-talk" ? "I just want to talk" : reason
                 }</option>)}
             </select>
+            {error && touched && <p>Error: Reason {error}</p>}
         </>
     )
 }
