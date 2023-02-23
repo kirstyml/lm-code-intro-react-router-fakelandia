9b4dd8f93382fffbaea9ccc3df22dad049337492
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-23 15:23:47 +0000

    footer styling

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,6 +1,9 @@
+import { ReactComponent as Logo } from "../assets/Fakelandia.svg";
+
 export const Footer : React.FC = () => {
     return (
         <footer className="footer">
+            <Logo height={100} width={100} />
             <p className="footer__copyright">© 2023 TechReturners</p>
         </footer>
     )
