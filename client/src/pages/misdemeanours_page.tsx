b00d840946c7d42a5041178e7823e6b764559c1e
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-24 11:30:58 +0000

    removed punishment provider so that it dynamically update when misdemeanours updates - as only being accessed in one place no need for context

--- client/src/pages/misdemeanours_page.tsx
+++ client/src/pages/misdemeanours_page.tsx
@@ -1,14 +1,35 @@
+import React, { useState, useEffect } from "react";
 import { MisdemeanoursContainer } from "../components/misdemeanours_container";
 import {
   useMisdemeanours,
   useMisdemeanoursLoading,
 } from "../context/misdemeanour_provider";
-import { usePunishments } from "../context/punishment_provider";
+import { Punishment } from "../types/punishment.types";
 
 export const Misdemeanours: React.FC = () => {
   const misdemeanours = useMisdemeanours();
-  const punishments = usePunishments();
   const misdemeanoursLoading = useMisdemeanoursLoading();
+  const [punishments, setPunishments] = useState<Punishment[]>([])
+
+  useEffect(() => {
+    if(misdemeanours) {
+      getPunishments(misdemeanours.length);
+    }
+  }, [misdemeanours]);
+
+  const getPunishments = async (amount: number) => {
+    try {
+      const response = await fetch(
+        `https://picsum.photos/v2/list?page=1&limit=${amount}`
+      );
+      if (response.status === 200 || response.status === 201) {
+        const responseJSON = await response.json();
+        setPunishments(responseJSON);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
